633a2ece33bb437888a9b4683224cedb153e5e17
cwwang <cwwang@68b9e3aa-a920-11de-ba6e-69e41c8db662>
2009-10-06 14:04:40 +0000

    database fix

--- trunk/src/com/cwandt/android/crowsFlight/NotesDbAdapter.java
+++ trunk/src/com/cwandt/android/crowsFlight/NotesDbAdapter.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2008 Google Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 
 package com.cwandt.android.crowsFlight;
 
@@ -55,7 +40,7 @@ public class NotesDbAdapter {
 
     private static final String DATABASE_NAME = "data";
     private static final String DATABASE_TABLE = "notes";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 4;
 
     private final Context mCtx;
 
